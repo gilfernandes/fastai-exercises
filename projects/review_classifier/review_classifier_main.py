38dc0e538c243c7f9c1e476b3e2a8a698ac3942f
Gil Fernandes <gil.fernandes@gmail.com>
2019-11-17 20:52:40 +0000

    First version of the review classifier
    Former-commit-id: 08a437c7384b8bdac1ad20753623081210871b7c

---
+++ projects/review_classifier/review_classifier_main.py
@@ -0,0 +1,44 @@
+from flask import Flask, escape, request
+from flask import jsonify
+from flask import render_template
+from fastai.text import *
+from pathlib import Path
+from flask import send_from_directory
+
+app = Flask(__name__, static_folder='text-classifier-ui/static', static_url_path = '/static', template_folder='text-classifier-ui')
+
+bs=12
+path = Path('/root/.fastai/data/yelp_review_full_csv')
+data_clas = load_data(path, 'data_clas.pkl', bs=bs)
+data_lm = load_data(path, 'data_lm.pkl', bs=bs)
+learn = text_classifier_learner(data_clas, AWD_LSTM, drop_mult=0.5)
+learn_lm = language_model_learner(data_lm, AWD_LSTM, drop_mult=0.3)
+learn.load_encoder('fine_tuned_enc')
+learn.load('fully_trained')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predict')
+def predict():
+    sentence = request.args.get("sentence", "")
+    pred_class, pred_idx, outputs = learn.predict(sentence)
+    response = jsonify({"response": sentence, "prediction": str(learn.predict(sentence)), "pred_class": int(str(pred_class))})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+@app.route('/generate')
+def generate():
+    text = request.args.get("text", "")
+    n_words = request.args.get("n_words", 120)
+    n_sentences = request.args.get("n_sentences", 1)
+    result = (learn_lm.predict(text, n_words, temperature=0.75) for _ in range(n_sentences))
+    response = jsonify({"text": "\n".join(result)})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+@app.route('/assets/images/<path:filename>')
+def custom_static(filename):
+    return send_from_directory('text-classifier-ui/assets/images', filename)
+
---
+++ projects/review_classifier/start.sh
@@ -0,0 +1,1 @@
+env FLASK_APP=review_classifier_main.py FLASK_DEBUG=1 flask run --host=0.0.0.0 --port=80
